0440159929e3f5a01d525c6084d900ea9b2db30d
Björn Norrliden <bjorn.norrliden@gmail.com>
2025-07-14 18:34:26 +0200

    feat: OnOffChip more color blind friendly

--- src/app/screens/device/screens/configuration/components/OnOffChip.tsx
+++ src/app/screens/device/screens/configuration/components/OnOffChip.tsx
@@ -1,39 +1,52 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Chip} from 'react-native-paper';
+import {StyleSheet, View} from 'react-native';
+import {Text, useTheme} from 'react-native-paper';
 
 interface OnOffChipProps {
   on: boolean;
 }
 
 export default function OnOffChip(props: OnOffChipProps) {
-  return props.on ? (
-    <Chip style={styles.chipOnStyle} textStyle={styles.textStyle}>
-      På
-    </Chip>
-  ) : (
-    <Chip style={styles.chipOffStyle} textStyle={styles.textStyle}>
-      Av
-    </Chip>
+  const {colors} = useTheme();
+  return (
+    <View
+      style={[
+        styles.chipStyle,
+        {
+          backgroundColor: props.on ? '#4CAF50' : colors.surfaceDisabled,
+        },
+      ]}>
+      <Text
+        style={[
+          styles.textStyle,
+          {
+            color: props.on ? 'white' : colors.onSurfaceDisabled,
+          },
+        ]}>
+        {props.on ? 'På' : 'Av'}
+      </Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  chipStyle: {
+    marginLeft: 10,
+    padding: 0,
+    height: 28,
+    width: 28,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    borderRadius: 20,
+  },
   textStyle: {
     color: 'white',
     fontWeight: 'bold',
-    fontSize: 12,
-  },
-  chipOnStyle: {
-    backgroundColor: 'green',
-    marginLeft: 10,
+    fontSize: 10,
     padding: 0,
-    height: 29,
-  },
-  chipOffStyle: {
-    backgroundColor: '#d00000',
-    marginLeft: 10,
-    padding: 0,
-    height: 29,
+    margin: 0,
+    textTransform: 'uppercase',
+    letterSpacing: 1.5,
   },
 });
