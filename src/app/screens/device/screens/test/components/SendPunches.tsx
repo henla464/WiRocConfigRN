7bb5707b853855a058d43eccbd4677de783ebfc0
Björn Norrliden <bjorn.norrliden@gmail.com>
2025-07-14 14:28:29 +0200

    feat: test - ui updates, fix dark theme issues
    Refs: #19

--- src/app/screens/device/screens/test/components/SendPunches.tsx
+++ src/app/screens/device/screens/test/components/SendPunches.tsx
@@ -1,8 +1,14 @@
 import {useQuery, useQueryClient} from '@tanstack/react-query';
 import React, {useEffect, useRef, useState} from 'react';
 import {ScrollView, StyleSheet, View} from 'react-native';
-import {SelectList} from 'react-native-dropdown-select-list';
-import {Button, DataTable, Divider, Icon, TextInput} from 'react-native-paper';
+import {
+  Button,
+  DataTable,
+  Divider,
+  Surface,
+  TextInput,
+  useTheme,
+} from 'react-native-paper';
 
 import {TestPunch} from '@api/types';
 import {useActiveWiRocDevice} from '@lib/hooks/useActiveWiRocDevice';
@@ -15,9 +21,11 @@ export default function SendPunches() {
   const queryClient = useQueryClient();
   const wiRocDeviceApi = useWiRocDeviceApi(deviceId);
   const [siCardNo, setSiCardNo] = useState<string>('16777215');
-  const [numberOfPunches, setNumberOfPunches] = useState(1);
+  const [numberOfPunchesInput, setNumberOfPunchesInput] = useState('1');
+  const numberOfPunches = parseInt2(numberOfPunchesInput);
+  const [sendIntervalInput, setSendIntervalInput] = useState('1');
+  const sendInterval = parseFloat(sendIntervalInput);
   const [isSending, setIsSending] = useState(false);
-  const [sendInterval, setSendInterval] = useState(1000);
   const stateRef = useRef<TestPunch[]>();
   const notify = useNotify();
 
@@ -33,34 +41,6 @@ export default function SendPunches() {
 
   // make stateRef always have the current punches
   stateRef.current = punches;
-
-  const sendNumberList = [
-    {key: '1', value: '1'},
-    {key: '2', value: '2'},
-    {key: '3', value: '3'},
-    {key: '4', value: '4'},
-    {key: '5', value: '5'},
-    {key: '6', value: '6'},
-    {key: '7', value: '7'},
-    {key: '8', value: '8'},
-    {key: '10', value: '10'},
-    {key: '15', value: '15'},
-    {key: '20', value: '20'},
-    {key: '30', value: '30'},
-    {key: '50', value: '50'},
-    {key: '100', value: '100'},
-  ];
-  const sendIntervalList = [
-    {key: '500', value: '0,5 s'},
-    {key: '1000', value: '1 s'},
-    {key: '1500', value: '1,5 s'},
-    {key: '2000', value: '2 s'},
-    {key: '3000', value: '3 s'},
-    {key: '4000', value: '4 s'},
-    {key: '5000', value: '5 s'},
-    {key: '10000', value: '10 s'},
-    {key: '15000', value: '15 s'},
-  ];
 
   const {data: ackReq} = useWiRocPropertyQuery(
     deviceId,
@@ -83,6 +63,7 @@ export default function SendPunches() {
       return punch.Type === 'TestPunch';
     });
     let noOfCompletedRows = completedPunches.length;
+
     if (
       testPunches.length === numberOfPunches &&
       numberOfPunches === noOfCompletedRows
@@ -95,12 +76,29 @@ export default function SendPunches() {
 
   const startStopSendPunches = async () => {
     if (isSending) {
-      wiRocDeviceApi.stopWatchingTestPunches();
       setIsSending(false);
+      wiRocDeviceApi.stopWatchingTestPunches();
     } else {
+      setIsSending(true);
       if (!siCardNo || siCardNo.length === 0 || isNaN(parseInt(siCardNo, 10))) {
         notify({
           message: 'SI Nummer måste fyllas i',
+          type: 'info',
+        });
+        return;
+      }
+
+      if (isNaN(numberOfPunches)) {
+        notify({
+          message: 'Ogiltigt antal',
+          type: 'info',
+        });
+        return;
+      }
+
+      if (isNaN(sendInterval)) {
+        notify({
+          message: 'Ogiltigt intervall',
           type: 'info',
         });
         return;
@@ -110,11 +108,9 @@ export default function SendPunches() {
       wiRocDeviceApi.startWatchingTestPunches();
       wiRocDeviceApi.startSendingTestPunches({
         numberOfPunches,
-        sendInterval,
+        sendInterval: sendInterval * 1000,
         siCardNo,
       });
-
-      setIsSending(true);
     }
   };
 
@@ -135,12 +131,28 @@ export default function SendPunches() {
     }
   };
 
+  const {colors} = useTheme();
   return (
-    <View style={styles.container}>
-      <View style={styles.containerRow}>
+    <View
+      style={{
+        padding: 16,
+        gap: 16,
+        flex: 1,
+        backgroundColor: colors.background,
+      }}>
+      <Surface
+        style={{
+          padding: 16,
+          flexDirection: 'row',
+          justifyContent: 'flex-start',
+          alignItems: 'flex-end',
+          flexWrap: 'wrap',
+          gap: 12,
+        }}>
         <TextInput
+          disabled={isSending}
           value={siCardNo}
-          label="SportIdent-nummer"
+          label="SI-nummer"
           onChangeText={text => {
             if (text === '') {
               setSiCardNo('');
@@ -150,70 +162,47 @@ export default function SendPunches() {
               setSiCardNo(siNumberParsed.toString());
             }
           }}
+          mode="outlined"
           keyboardType="number-pad"
-          style={{flex: 1, backgroundColor: 'rgb(255, 251, 255)'}}
+          style={{width: '100%'}}
         />
-        <SelectList
-          setSelected={(val: string) => {
-            setNumberOfPunches(parseInt(val, 10));
-          }}
-          data={sendNumberList}
-          save="key"
-          search={false}
-          placeholder={'Antal'}
-          dropdownTextStyles={{fontSize: 30, flex: 1}}
-          dropdownStyles={{backgroundColor: 'rgb(255, 251, 255)'}}
-          inputStyles={{
-            fontSize: 18,
-            fontWeight: '900',
-          }}
-          boxStyles={{
-            width: 150,
-            alignItems: 'center',
-            height: 60,
-            marginRight: 0,
-            marginLeft: 5,
-          }}
-          arrowicon={<Icon source="chevron-down" size={35} color={'black'} />}
-          defaultOption={{key: '1', value: '1'}}
+        <TextInput
+          disabled={isSending}
+          value={numberOfPunchesInput}
+          label="Antal"
+          error={isNaN(numberOfPunches)}
+          selectTextOnFocus
+          onChangeText={setNumberOfPunchesInput}
+          mode="outlined"
+          keyboardType="number-pad"
+          style={{flex: 1}}
         />
-      </View>
-      <View style={styles.containerRow}>
-        <SelectList
-          setSelected={(val: string) => {
-            setSendInterval(parseInt(val, 10));
-          }}
-          data={sendIntervalList}
-          save="key"
-          search={false}
-          placeholder={'Interval'}
-          dropdownTextStyles={{fontSize: 30, flex: 1}}
-          dropdownStyles={{backgroundColor: 'rgb(255, 251, 255)'}}
-          inputStyles={{
-            fontSize: 18,
-            fontWeight: '900',
-          }}
-          boxStyles={{
-            width: 150,
-            alignItems: 'center',
-            height: 60,
-            marginRight: 5,
-            marginLeft: 0,
-          }}
-          arrowicon={<Icon source="chevron-down" size={35} color={'black'} />}
-          defaultOption={{key: '500', value: '0,5 s'}}
+        <TextInput
+          disabled={isSending}
+          value={sendIntervalInput}
+          label="Intervall"
+          right={<TextInput.Affix text="s" />}
+          error={isNaN(sendInterval)}
+          selectTextOnFocus
+          onChangeText={setSendIntervalInput}
+          mode="outlined"
+          keyboardType="number-pad"
+          style={{flex: 1}}
         />
         <Button
-          icon=""
+          hitSlop={{top: 10, bottom: 10}}
+          icon="send"
           loading={isSending}
           mode="contained"
+          buttonColor={isSending ? 'red' : 'green'}
+          color="secondary"
           onPress={startStopSendPunches}
-          style={[styles.button, {flex: 1, marginRight: 0}]}>
+          labelStyle={{fontSize: 16}}
+          style={{flex: 1}}>
           {isSending ? 'Sluta skicka' : 'Skicka'}
         </Button>
-      </View>
-      <Divider bold={true} style={{marginTop: 5, height: 2}} />
-      <View style={styles.tableContainer}>
+      </Surface>
+      <Surface style={{flex: 1}}>
         <DataTable style={styles.table}>
           <DataTable.Header style={styles.row}>
             <DataTable.Title textStyle={{fontSize: 20}} style={{flex: 14}}>
@@ -242,107 +231,103 @@ export default function SendPunches() {
           </DataTable.Header>
           <Divider bold={true} />
           <ScrollView contentContainerStyle={{flexGrow: 1}} style={{}}>
-            <View>
-              {punches.map(punch => (
-                <DataTable.Row key={punch.Id} style={styles.row}>
-                  <DataTable.Cell
-                    textStyle={{fontSize: 22}}
-                    style={[
-                      {flex: 14},
-                      punch.Type === 'Punch'
-                        ? styles.punchBackgroundColor
-                        : styles.testPunchBackgroundColor,
-                    ]}>
-                    {punch.SINo}
-                  </DataTable.Cell>
-                  <DataTable.Cell
-                    textStyle={{fontSize: 20}}
-                    style={[
-                      punch.Type === 'Punch'
-                        ? styles.punchBackgroundColor
-                        : styles.testPunchBackgroundColor,
-                      styles.centered,
-                      {flex: 12},
-                    ]}>
-                    {punch.Time}
-                  </DataTable.Cell>
-                  <DataTable.Cell
-                    textStyle={{fontSize: 22}}
-                    style={[
-                      {flex: 6, justifyContent: 'center'},
-                      punch.Type === 'Punch'
-                        ? styles.punchBackgroundColor
-                        : styles.testPunchBackgroundColor,
-                    ]}>
-                    {punch.RSSI}
-                  </DataTable.Cell>
-                  <DataTable.Cell
-                    textStyle={{fontSize: 22}}
-                    style={[
-                      punch.Type === 'Punch'
-                        ? [styles.punchBackgroundColor, styles.centered]
-                        : punch.NoOfSendTries > 1
+            {punches.map(punch => (
+              <DataTable.Row key={punch.Id} style={styles.row}>
+                <DataTable.Cell
+                  textStyle={{fontSize: 22}}
+                  style={[
+                    {flex: 14},
+                    punch.Type === 'Punch'
+                      ? styles.punchBackgroundColor
+                      : styles.testPunchBackgroundColor,
+                  ]}>
+                  {punch.SINo}
+                </DataTable.Cell>
+                <DataTable.Cell
+                  textStyle={{fontSize: 20}}
+                  style={[
+                    punch.Type === 'Punch'
+                      ? styles.punchBackgroundColor
+                      : styles.testPunchBackgroundColor,
+                    styles.centered,
+                    {flex: 12},
+                  ]}>
+                  {punch.Time}
+                </DataTable.Cell>
+                <DataTable.Cell
+                  textStyle={{fontSize: 22}}
+                  style={[
+                    {flex: 6, justifyContent: 'center'},
+                    punch.Type === 'Punch'
+                      ? styles.punchBackgroundColor
+                      : styles.testPunchBackgroundColor,
+                  ]}>
+                  {punch.RSSI}
+                </DataTable.Cell>
+                <DataTable.Cell
+                  textStyle={{fontSize: 22}}
+                  style={[
+                    punch.Type === 'Punch'
+                      ? [styles.punchBackgroundColor, styles.centered]
+                      : punch.NoOfSendTries > 1
                         ? styles.failure
                         : punch.Status === 'Acked'
-                        ? styles.success
-                        : styles.centered,
-                      {flex: 7},
-                    ]}>
-                    {punch.NoOfSendTries}
-                  </DataTable.Cell>
-                  <DataTable.Cell
-                    textStyle={{fontSize: 20}}
-                    style={[
-                      punch.Type === 'Punch'
-                        ? styles.punchBackgroundColor
-                        : !ackReq
+                          ? styles.success
+                          : styles.centered,
+                    {flex: 7},
+                  ]}>
+                  {punch.NoOfSendTries}
+                </DataTable.Cell>
+                <DataTable.Cell
+                  textStyle={{fontSize: 20}}
+                  style={[
+                    punch.Type === 'Punch'
+                      ? styles.punchBackgroundColor
+                      : !ackReq
                         ? null
                         : punch.Status === 'Acked'
-                        ? styles.success
-                        : punch.Status === 'Not acked'
-                        ? styles.failure
-                        : styles.centered,
-                      {flex: 10},
-                    ]}>
-                    {getStatusDisplayName(punch.Status)}
-                  </DataTable.Cell>
-                </DataTable.Row>
-              ))}
-              <Divider bold={true} />
-              <DataTable.Row key={'footer'} style={styles.row}>
-                <DataTable.Cell textStyle={{fontSize: 22}} style={{flex: 31}}>
-                  Procent lyckade
-                </DataTable.Cell>
-                <DataTable.Cell
-                  textStyle={{fontSize: 22}}
-                  style={{flex: 8, justifyContent: 'center'}}>
-                  {Math.round(
-                    (100 * punches.filter(p => p.Status === 'Acked').length) /
-                      punches
-                        .filter(punch => {
-                          return punch.Status !== 'Punch';
-                        })
-                        .map(p => p.NoOfSendTries)
-                        .reduce((a, b) => a + b, 0),
-                  )}
-                  {'%'}
-                </DataTable.Cell>
-                <DataTable.Cell
-                  textStyle={{fontSize: 22}}
-                  style={{flex: 10, justifyContent: 'center'}}>
-                  {Math.round(
-                    (100 * punches.filter(p => p.Status === 'Acked').length) /
-                      punches.filter(punch => {
-                        return punch.Status !== 'Punch';
-                      }).length,
-                  )}
-                  {'%'}
+                          ? styles.success
+                          : punch.Status === 'Not acked'
+                            ? styles.failure
+                            : styles.centered,
+                    {flex: 10},
+                  ]}>
+                  {getStatusDisplayName(punch.Status)}
                 </DataTable.Cell>
               </DataTable.Row>
-            </View>
+            ))}
           </ScrollView>
+          <Divider bold={true} />
+          <DataTable.Row key={'footer'} style={styles.row}>
+            <DataTable.Cell textStyle={{fontSize: 22}} style={{flex: 31}}>
+              Procent lyckade
+            </DataTable.Cell>
+            <DataTable.Cell
+              textStyle={{fontSize: 22}}
+              style={{flex: 8, justifyContent: 'center'}}>
+              {formatPercentage(
+                punches.filter(p => p.Status === 'Acked').length /
+                  punches
+                    .filter(punch => {
+                      return punch.Status !== 'Punch';
+                    })
+                    .map(p => p.NoOfSendTries)
+                    .reduce((a, b) => a + b, 0),
+              )}
+            </DataTable.Cell>
+            <DataTable.Cell
+              textStyle={{fontSize: 22}}
+              style={{flex: 10, justifyContent: 'center'}}>
+              {formatPercentage(
+                punches.filter(p => p.Status === 'Acked').length /
+                  punches.filter(punch => {
+                    return punch.Status !== 'Punch';
+                  }).length,
+              )}
+            </DataTable.Cell>
+          </DataTable.Row>
         </DataTable>
-      </View>
+      </Surface>
     </View>
   );
 }
@@ -354,13 +339,8 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   row: {
-    paddingRight: 5,
-    paddingLeft: 5,
-  },
-  tableContainer: {
     paddingRight: 0,
-    marginTop: 0,
-    flex: 1,
+    paddingLeft: 5,
   },
   success: {
     backgroundColor: 'palegreen',
@@ -373,29 +353,22 @@ const styles = StyleSheet.create({
   centered: {
     justifyContent: 'center',
   },
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    paddingLeft: 5,
-    paddingTop: 0,
-    paddingRight: 5,
-    paddingBottom: 0,
-    backgroundColor: 'rgb(255, 251, 255)',
-  },
-  containerRow: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    paddingLeft: 0,
-    paddingTop: 8,
-    paddingRight: 0,
-    paddingBottom: 0,
-  },
-  button: {
-    padding: 10,
-  },
   punchBackgroundColor: {
     backgroundColor: 'lightgray',
   },
   testPunchBackgroundColor: {},
 });
+
+function parseInt2(value: string) {
+  if (!/^\d+$/.test(value)) {
+    return NaN;
+  }
+  return parseInt(value, 10);
+}
+
+function formatPercentage(value: number) {
+  if (isNaN(value)) {
+    return '-';
+  }
+  return `${Math.round(value * 100)}%`;
+}
