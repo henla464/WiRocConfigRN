d5e9dab0ca503c96be02ab6928e156737ebb6cac
Henrik Larsson <laselase@gmail.com>
2026-01-31 00:51:22 +0100

    etc

--- src/app/screens/about/index.tsx
+++ src/app/screens/about/index.tsx
@@ -13,7 +13,7 @@ export default function AboutScreen() {
             WiRoc Config
           </Text>
           <Text style={{fontSize: 20, fontWeight: 'bold', paddingTop: 14}}>
-            Version 19
+            Version 20
           </Text>
         </View>
         <View style={styles.containerColumn}>
