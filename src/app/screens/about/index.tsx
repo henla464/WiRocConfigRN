bfa682c776f2c0c8ffd73b21a522255bfa962f1e
Henrik Larsson <laselase@gmail.com>
2026-09-11 19:12:25 +0200

    change version number

--- src/app/screens/about/index.tsx
+++ src/app/screens/about/index.tsx
@@ -13,7 +13,7 @@ export default function AboutScreen() {
             WiRoc Config
           </Text>
           <Text style={{fontSize: 20, fontWeight: 'bold', paddingTop: 14}}>
-            Version 23
+            Version 24
           </Text>
         </View>
         <View style={styles.containerColumn}>
